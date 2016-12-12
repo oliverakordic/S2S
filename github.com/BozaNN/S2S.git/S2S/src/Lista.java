a8d10ad206abc5f2904411f869e9f35fed8b9e84
akikasaki <alektramp@gmail.com>
2016-12-12 21:09:04 +0100

    Hello

---
+++ github.com/BozaNN/S2S.git/S2S/src/Lista.java
@@ -0,0 +1,5 @@
+import java.util.LinkedList;
+
+public class Lista {
+	public LinkedList<Student> studenti = new LinkedList<Student>();
+}
--- github.com/jovanastankovic/s2s.git/projekat1/src/projekat1/Student.java
+++ github.com/jovanastankovic/s2s.git/projekat1/src/projekat1/Student.java
@@ -2,4 +2,35 @@ package projekat1;
 
 public class Student {
 
+	
+	
+	public Student(String ime,String prezime,int br) {
+		super();
+		this.ime=ime;
+		this.prezime=prezime;
+				brIndeksa=br;
+		
+	}
+	String ime;
+	public String getIme() {
+		return ime;
+	}
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+	public String getPrezime() {
+		return prezime;
+	}
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+	public int getBrIndeksa() {
+		return brIndeksa;
+	}
+	public void setBrIndeksa(int brIndeksa) {
+		this.brIndeksa = brIndeksa;
+	}
+	String prezime;
+	int brIndeksa;
+	
 }
