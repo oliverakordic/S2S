f2305dad0ecd9678cd8a01d05cb518a308e10570
BozaNN <lukadelija5@gmail.com>
2016-12-12 21:19:30 +0100

    Ae

--- github.com/BozaNN/S2S.git/S2S/src/Lista.java
+++ github.com/BozaNN/S2S.git/S2S/src/Lista.java
@@ -2,4 +2,10 @@ import java.util.LinkedList;
 
 public class Lista {
 	public LinkedList<Student> studenti = new LinkedList<Student>();
+	
+	public void napraviListu(Student s){
+		if(s==null){
+			syso
+		}
+	}
 }
